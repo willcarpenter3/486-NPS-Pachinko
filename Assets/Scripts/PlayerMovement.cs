f7cf4c160cf51e3540efd54a1492c4215f2902e6
Ryan Rice <rnrice2@ncsu.edu>
2022-02-07 00:23:35 -0500

    Limited the angle of the launcher; launcher fully implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public GameObject player;
+    public GameObject container;
+    public GameObject pointer;
     private Rigidbody rb;
     private float movementX;
 
@@ -20,37 +22,53 @@ public class PlayerMovement : MonoBehaviour
         //movementX = ctx.ReadValue<float>();
         Vector2 movementVector = movement.Get<Vector2>(); //movementX * (new Vector2(1f, 0f));
         movementX = movementVector.x;
-        Debug.Log(movementX);
     }
 
     private void OnDrop(InputValue drop)
     {
+        // Disable pointer
+        pointer.SetActive(false);
         // Turn on gravity
         rb.useGravity = true;
         // Disable player movement
         player.GetComponent<PlayerMovement>().enabled = false;
-        GameManager.Instance.setIsPlaying(true);
+        // GameManager.Instance.setIsPlaying(true);
+        // Launch the player in the direction of the pointer
+        rb.AddForce(pointer.transform.up * -1000f);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Peg")
         {
-            // Disable the collided object
-            collision.gameObject.SetActive(false);
+            // Disable the collided object after a short delay
+            StartCoroutine("DisablePeg", collision.gameObject);
         }
+    }
+
+    private IEnumerator DisablePeg(GameObject peg)
+    {
+        yield return new WaitForSeconds(0.5f);
+        // Disable the collided object after a short delay
+        peg.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX * 0.5f, 0.0f, 0.0f);
+        Vector3 movement = new Vector3(movementX, 0.0f, 0.0f);
 
-        // Check the bounds of the player
-        if (player.transform.rotation.y + movement.x <= 80.0f && player.transform.rotation.x + movement.y >= -80.0f)
-        {   
-            // Rotate the player
-            player.transform.Rotate(0.0f, movementX * -1.0f, 0.0f);
+        Vector3 currentRotation = container.transform.rotation.eulerAngles;
+        Vector3 newRotation = new Vector3(0.0f, 0.0f, currentRotation.z + movement.x);
+        
+        // Bound checking on the angle
+        float angle = newRotation.z;
+        angle = (angle > 180) ? angle - 360 : angle;
+        if (angle >= -80.0f && angle <= 80.0f)
+        {
+            // Rotate the container
+            container.transform.rotation = Quaternion.Euler(newRotation);
         }
+
     }
 }
